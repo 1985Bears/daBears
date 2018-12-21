a2de54bcc41b6f881f3897ad38d9cff059eae109
Sergio Jr <sergio23jr@gmail.com>
2018-12-20 20:57:22 -0600

    updated html routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,38 @@
 var db = require("../models");
 
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+module.exports = function (app) {
+  // Get all users
+  app.get("/api/users", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.json(dbExamples);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
+  app.post("/api/examples", function (req, res) {
+    db.Example.create(req.body).then(function (dbExample) {
       res.json(dbExample);
     });
   });
 
+  //Creates a new user 
+  app.post("/api/user", function (req, res) {
+    db.Example.create(req.body).then(function (newUser) {
+      res.json(newUser);
+    });
+  });
+
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.delete("/api/examples/:id", function (req, res) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
       res.json(dbExample);
+    });
+  });
+
+  //delete a old user
+  app.delete("/api/examples/:username", function (req, res) {
+    db.Example.destroy({ where: { user_name: req.params.username } }).then(function (user) {
+      res.json(user);
     });
   });
 };
