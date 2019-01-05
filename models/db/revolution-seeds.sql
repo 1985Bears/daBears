4f03255c77c467378ebf07968adb021b30ad9b85
Ben deLong <bendelong45@gmail.com>
2019-01-04 20:21:30 -0600

    updated revolution seeds with category

--- models/db/revolution-seeds.sql
+++ models/db/revolution-seeds.sql
@@ -4,41 +4,46 @@ VALUES ('Revolution Brewing', '3340 N. Kedzie Ave Chicago, IL 60618');
 
 
 -- INSERT INTO BEERS 
-INSERT INTO beers (beer_name, style, abv, brew_id) VALUES
-('Anti-Hero', 'IPA', 6.7, 4), 
-('Eugene', 'Porter', 6.8, 4), 
-('Rev', 'Pilsner', 5.5,  4), 
-('Fist City', 'Pale Ale', 5.5,  4), 
-('Cross of Gold', 'Gold Ale', 4.8, 4), 
-('A Little Crazy', 'Belgian Ale', 6.8, 4);
 
-INSERT INTO beers (beer_name, style, abv, description, brew_id) VALUES
-('Sun Crusher', 'Wheat Ale', 5.3, 'A crushable ale to celebrate the end of winter and rejoice the coming of warm weather! This Hoppy Wheat Ale is brewed with Apollo and Amarillo hops, with a dry-hop blend of Crystal, Amarillo, and Mosaic, resulting in a mellow bitterness and pronounced flowery and citrus aromas.', 4), 
+INSERT INTO beers (category, beer_name, style, abv, description, brew_id) VALUES
+('IPA', 'Anti-Hero', 'IPA', 6.7, 'Our flagship IPA is supremely aromatic, crisp, and drinkable. This iconic ale features a blend of Citra, Crystal, Centennial, and Chinook to create a crisp clean bitterness and imparts massive floral and citrus aromas.  An American hop assault for all the ambivalent warriors who get the girl in the end. “Look, I aint in this for your revolution, and Im not in it for you, Princess.”', 4),
 
-('Oktoberfest', 'German Ale', 5.7, 'Our Oktoberfest Bier is a German-style  lager that was brewed in the summer and “cold stored” until late August to celebrate the coming of fall!  Traditional German malts such as Pilsner, Vienna, Carared and Munich lend a beautiful burnt orange color and a pleasant toasty malt flavor and aroma to this lager.  Initial Magnum hopping along with multiple additions of German Saphir and Select hops throughout the boil provide a crisp balanced bitterness and spicy/earthy aroma to round out this robust beer! For fermentation we use a Bavarian Lager yeast and then lower the temperature to 32 °F and store the beer cold for 4 weeks.  This cold maturation time helps provide a smooth round mouthfeel and clean crisp finish.', 4), 
+('Porter/Stout', 'Eugene', 'Porter', 6.8, 'A striking, robust porter full of warmth and chocolate malt. An assortment of Belgian specialty malts form a complex structure of toasted grain and caramel flavors. Dark chocolate malt makes this porter black as night and infuses it with its distinct intense, chocolate essence.  Eugene is a robust porter named after Eugene V. Debs, an American union leader and activist who led the Pullman Railroad strike in 1894.', 4),
 
-('Fistmas', 'Red Ale', 6.5, 'Our Holiday Red Ale is steeped with ginger and orange peel. It features notes of fresh-baked bread, caramel, and stone fruit along with holiday cheer.', 4), 
+('Pilsner', 'Rev', 'Pilsner', 5.5, 'Our Chicago Pilsner. Brewed traditionally German, with a slightly American approach. All German hops and malts come together for an exceptionally clean, crisp pilsner.  Our can features statues designed by Frank Lloyd Wright, a famous Chicago architect. These sculptures, also known as "Maid in the Mud",  were located in the famous Midway Gardens in Chicago, Illinois.',  4),
 
-('Galaxy-Hero', 'IPA', 7.0, 'Brewed for the Chicago Comic and Entertainment Expo, this IPA shoots a raygun blast of Galaxy hops into your mouth and leaves a crisp, dry finish. This IPA has big hop flavors and aromas of tropical fruit and citrus.', 4), 
+('Pale Ale', 'Fist City', 'Pale Ale', 5.5, 'A Pale Ale brewed with an abundant amount of 2 row and red wheat with the addition of specialty malt for a beautiful golden hue. Fist City has hints of citrus and grapefruit notes with a solid malt backbone to smooth the light bite of hops. This Chicago Pale Ale is meant to be a supremely drinkable brew for those who love hops. Magnificent California hop aromas play in perfect harmony with this beer’s extremely satisfying finish. The Statue of the Republic, found in Jackson Park, Chicago, is featured on this can. It is a smaller replica of the original Statue of the Republic, which was constructed for the 1893 Worlds Columbian Exposition in Chicago.',  4),
 
-('Tropic-Hero', 'IPA', 7.0, 'This American IPA is brewed with 2-row and Pale malts to establish a smooth malt profile and golden color.', 4), 
+('Wheat Beer', 'Cross of Gold', 'Gold Ale', 4.8, 'An easy drinking golden ale for everyone.  Brewed to be crisp and refreshing with a bit of wheat malt for body and a delicate hop finish. William Jennings Bryan delivered one of the most famous political speeches ever at the 1896 Democratic National Convention, in Chicago. The speech was named Cross of Gold, and provided the inspiration for one of Revolutions original beers. Jennings Bryan was a populist, something we craft brewers consider ourselves to be. Cross of Gold was first brewed in Josh Deths basement by Brewmaster Jim Cibak, in June 2009. Two additional batches followed, to dial in the recipe, before becoming one of the initial beers served at the Revolution Brewpub in 2010. Cross of Gold has since taken home Gold at the Great American Beer Festival and the World Beer Cup. It remains a favorite among our employees, and is a top seller at the Brewpub and Tap Room.', 4),
 
-('Citra-Hero', 'IPA', 7.5, 'Citra-Hero is an elegant American IPA designed to showcase the Citra Hop variety. This IPA is exploding with citrusy flavors and aromas.', 4), 
+('Pale Ale', 'A Little Crazy', 'Belgian Ale', 6.8, 'A Pilsner malt base provides a nice toasty flavor with hints of caramel and enough fermentable sugar to make this a rather substantial beer. The Belgian-Style Pale Ale is brewed with Magnum, Cascade and Citra hops in the brewhouse and then dryhopped with a blend of Citra and Cascade. This drives home the aromas of fresh citrus rind to make this a very drinkable and extremely aromatic beer.', 4),
 
-('Crystal-Hero', 'IPA', 7.2, 'A crisp and refreshing IPA exploding with fresh floral hop character, named for a somewhat forgotten yet amazing hop variety named Crystal. A unique hopping program gives this beer a massive, clean bitterness and fresh hop aroma bursting with hints of citrus and fresh fruit.', 4),
+('Wheat Beer', 'Sun Crusher', 'Wheat Ale', 5.3, 'A crushable ale to celebrate the end of winter and rejoice the coming of warm weather! This Hoppy Wheat Ale is brewed with Apollo and Amarillo hops, with a dry-hop blend of Crystal, Amarillo, and Mosaic, resulting in a mellow bitterness and pronounced flowery and citrus aromas.', 4), 
 
-('Mosaic-Hero', 'IPA', 7.5, 'An exotic IPA brewed with 100% pale malt to yield a deep golden color and smooth mouthfeel. The dry-hop primarily features Mosaic hops, infusing tropical fruit flavors like mango and pineapple, along with crystal hops to add a touch of fresh pine.', 4),
+('Farmhouse/Saison', 'Oktoberfest', 'German Ale', 5.7, 'Our Oktoberfest Bier is a German-style  lager that was brewed in the summer and “cold stored” until late August to celebrate the coming of fall!  Traditional German malts such as Pilsner, Vienna, Carared and Munich lend a beautiful burnt orange color and a pleasant toasty malt flavor and aroma to this lager.  Initial Magnum hopping along with multiple additions of German Saphir and Select hops throughout the boil provide a crisp balanced bitterness and spicy/earthy aroma to round out this robust beer! For fermentation we use a Bavarian Lager yeast and then lower the temperature to 32 °F and store the beer cold for 4 weeks.  This cold maturation time helps provide a smooth round mouthfeel and clean crisp finish.', 4), 
 
-('Northwest-Hero', 'IPA', 7.1, 'IPA brewed to showcase the dank aroma hop varieties grown in the Pacific Northwest, including Columbus, Chinook, Centennial, Cascade and Warrior.', 4),
+('Sour/Fruit', 'Fistmas', 'Red Ale', 6.5, 'Our Holiday Red Ale is steeped with ginger and orange peel. It features notes of fresh-baked bread, caramel, and stone fruit along with holiday cheer.', 4), 
 
-('Jukebox-Hero', 'IPA', 7.5, 'Jukebox Hero, named after the well-known rock song, is a black IPA is bursting with crisp, clean bitterness and layers of wonderful American hop character.', 4),
+('IPA', 'Galaxy-Hero', 'IPA', 7.0, 'Brewed for the Chicago Comic and Entertainment Expo, this IPA shoots a raygun blast of Galaxy hops into your mouth and leaves a crisp, dry finish. This IPA has big hop flavors and aromas of tropical fruit and citrus.', 4), 
 
-('Local-Hero', 'IPA', 7.5, 'This immensely drinkable IPA is brewed with Centennial, Cascade, Chinook, and Nugget hops, all from our friends at Hop Heads Farms in Hickory Corners, MI.', 4),
+('IPA', 'Tropic-Hero', 'IPA', 7.0, 'This American IPA is brewed with 2-row and Pale malts to establish a smooth malt profile and golden color.', 4), 
 
-('Centennial-Hero', 'IPA', 7.0, 'This new IPA gives flight to Centennial, the Lord of the “C” hops, allowing the fresh citrus and earthy characteristics to soar.', 4),
+('IPA', 'Citra-Hero', 'IPA', 7.5, 'Citra-Hero is an elegant American IPA designed to showcase the Citra Hop variety. This IPA is exploding with citrusy flavors and aromas.', 4), 
 
-('Brut-Hero', 'IPA', 7, 'A new hero is soaring in from a completely new direction–the effervescent Brut-Hero, sparkling with white grape, passionfruit, and lemon notes and capped with an extra-dry champagne-like finish.', 4),
+('IPA', 'Crystal-Hero', 'IPA', 7.2, 'A crisp and refreshing IPA exploding with fresh floral hop character, named for a somewhat forgotten yet amazing hop variety named Crystal. A unique hopping program gives this beer a massive, clean bitterness and fresh hop aroma bursting with hints of citrus and fresh fruit.', 4),
 
-('Cashmere-Hero', 'IPA', 7.0, 'A mountain of flavors rise above this silky, Cashmere-hopped IPA-lemon, lime, and ripe cantaloupe with a touch of grapefruit.', 4);
+('IPA', 'Mosaic-Hero', 'IPA', 7.5, 'An exotic IPA brewed with 100% pale malt to yield a deep golden color and smooth mouthfeel. The dry-hop primarily features Mosaic hops, infusing tropical fruit flavors like mango and pineapple, along with crystal hops to add a touch of fresh pine.', 4),
+
+('IPA', 'Northwest-Hero', 'IPA', 7.1, 'IPA brewed to showcase the dank aroma hop varieties grown in the Pacific Northwest, including Columbus, Chinook, Centennial, Cascade and Warrior.', 4),
+
+('IPA', 'Jukebox-Hero', 'IPA', 7.5, 'Jukebox Hero, named after the well-known rock song, is a black IPA is bursting with crisp, clean bitterness and layers of wonderful American hop character.', 4),
+
+('IPA', 'Local-Hero', 'IPA', 7.5, 'This immensely drinkable IPA is brewed with Centennial, Cascade, Chinook, and Nugget hops, all from our friends at Hop Heads Farms in Hickory Corners, MI.', 4),
+
+('IPA', 'Centennial-Hero', 'IPA', 7.0, 'This new IPA gives flight to Centennial, the Lord of the “C” hops, allowing the fresh citrus and earthy characteristics to soar.', 4),
+
+('IPA', 'Brut-Hero', 'IPA', 7, 'A new hero is soaring in from a completely new direction–the effervescent Brut-Hero, sparkling with white grape, passionfruit, and lemon notes and capped with an extra-dry champagne-like finish.', 4),
+
+('IPA', 'Cashmere-Hero', 'IPA', 7.0, 'A mountain of flavors rise above this silky, Cashmere-hopped IPA-lemon, lime, and ripe cantaloupe with a touch of grapefruit.', 4);
 -- End of Rev Beers
 
