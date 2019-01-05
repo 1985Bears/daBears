c88a3197bad5e618b8fe9b91a37e7a2609524f0f
Ben deLong <bendelong45@gmail.com>
2019-01-05 17:59:59 -0600

    updated whiner seeds

--- models/db/whiner-seeds.sql
+++ models/db/whiner-seeds.sql
@@ -2,16 +2,17 @@
 INSERT INTO Breweries (name, location) VALUES ("Whiner","1400 W 46th St, Chicago, IL 60609");
 
 -- INSERT INTO BEERS 
-INSERT INTO beers (beer_name, style, abv, description, brew_id) VALUES
-('Le Tub', 'Saison', 6.4, 'White grapes, lemon tarts, funky fresh. You’ll want to bathe your mouth with it.', 8),
+INSERT INTO beers (category, beer_name, style, abv, description, brew_id) VALUES
 
-('Hell Kitty Kitty', 'Belgo-American Pale Ale', 7, 'Passion fruit and citrus aromatics, big time juicy fruits with a nice hoppy bite.', 8),
+('Farmhouse/Saison', 'Le Tub', 'Saison', 6.4, 'White grapes, lemon tarts, funky fresh. You’ll want to bathe your mouth with it.', 8),
 
-('Rubrique-A-Brac', 'Dry Hopped Bier de Garde', 5.5, 'Spices, ripe orange, light caramel, wild rustic leather notes after slight aging. Zesty, dry.', 8),
+('Pale Ale', 'Hell Kitty Kitty', 'Belgo-American Pale Ale', 7, 'Passion fruit and citrus aromatics, big time juicy fruits with a nice hoppy bite.', 8),
 
-('Miaou', 'Barrel Aged Belgian Wheat', 6.5, 'Juicy tangerine notes, lightly hoppy, refreshing', 8),
+('Sour/Fruit', 'Rubrique-A-Brac', 'Dry Hopped Bier de Garde', 5.5, 'Spices, ripe orange, light caramel, wild rustic leather notes after slight aging. Zesty, dry.', 8),
 
-('Bubble Tub', 'Watermelon Saison Kettle Sour', 6.2, 'This Kettle-Soured Saison is loaded with fresh watermelons. Tart, dry and as the kids say nowadays, refreshing AF.', 8),
+('Sour/Fruit', 'Miaou', 'Barrel Aged Belgian Wheat', 6.5, 'Juicy tangerine notes, lightly hoppy, refreshing', 8),
+
+('Sour/Fruit', 'Bubble Tub', 'Watermelon Saison Kettle Sour', 6.2, 'This Kettle-Soured Saison is loaded with fresh watermelons. Tart, dry and as the kids say nowadays, refreshing AF.', 8),
 
 
 
