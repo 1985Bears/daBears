36a66debd7a960ec84af03c110c350d4e8d7add6
Ben deLong <bendelong45@gmail.com>
2019-01-04 20:20:46 -0600

    updated hopewell seeds with category

--- models/db/hopewell-seeds.sql
+++ models/db/hopewell-seeds.sql
@@ -3,24 +3,22 @@ INSERT INTO breweries (name, location) VALUES ('Hopewell', "2760 N. Milwaukee, C
 
 
 -- INSERT INTO BEERS 
-INSERT INTO beers (beer_name, style, abv, description, brew_id) VALUES ('First Lager', 'Lager', 5.1, 'A first and a favorite. Delightfully crisp, balancing bready malt with pronounced noble hop aroma. This is a year-round beer, good from the dog days of summer through the never-ending winter. Also available in 6-pack cans!', 2);
+INSERT INTO beers (category, beer_name, style, abv, description, brew_id) VALUES ('Farmhouse/Saison', 'First Lager', 'Lager', 5.1, 'A first and a favorite. Delightfully crisp, balancing bready malt with pronounced noble hop aroma. This is a year-round beer, good from the dog days of summer through the never-ending winter. Also available in 6-pack cans!', 2),
 
-INSERT INTO
-beers (beer_name, style, abv, description, brew_id) VALUES ('Swift IPA', 'India Pale Ale', 6.9, 'Swift is our flagship IPA, featuring a substantial crisp bitterness kept in check by a solid malt base. Featuring Amarillo, Simcoe and Centennial hops, Swift is a bright and clean IPA with an intense citrus bouquet and flavor.', 2);
-INSERT INTO
-beers (beer_name, style, abv, description, brew_id) VALUES ('Ya Filthy Animal', 'Kettle Sour', 6.5, 'Inspired by the warming flavors of mulled wine. Ya Filthy Animal is a kettle sour brewed with plum, black currants and mulling spices. Keep the change.', 2);
-INSERT INTO
-beers (beer_name, style, abv, description, brew_id) VALUES ('Ride or Die', 'Pale Ale', 5.5, 'A mellow, sessionable pale ale with tropical fruit overtones and an undercurrent of citrus zest. Built for the dog days, but good no matter what.', 2);
-INSERT INTO
-beers (beer_name, style, abv, description, brew_id) VALUES ('Clique', 'Brut IPA', 6.5, 'A Brut IPA with big notes of citrus zest, followed by a bone-dry finish.', 2);
-INSERT INTO
-beers (beer_name, style, abv, description, brew_id) VALUES ('There There', 'Double India Pale Ale', 7.7, 'A juicy, dank DIPA soaked with heavy doses of Vic Secret and Wakatu hops and fermented with a fruity Norwegian yeast strain.', 2);
-INSERT INTO
-beers (beer_name, style, abv, description, brew_id) VALUES ('24:37 Red IPA', 'India Pale Ale', 6.5, 'With a notable red tint from roasted malt, this IPA has a bright, clean hop aroma and bitterness. Dry-hopped with Sorachi Ace for a floral, lemongrass aroma.', 2);
-INSERT INTO
-beers (beer_name, style, abv, description, brew_id) VALUES ('Off-Black', 'Dark Lager', 5.5, 'A roasty and malty Schwarzbier that is kept in balance with a healthy dose of noble hops.', 2);
-INSERT INTO
+('IPA', 'Swift IPA', 'India Pale Ale', 6.9, 'Swift is our flagship IPA, featuring a substantial crisp bitterness kept in check by a solid malt base. Featuring Amarillo, Simcoe and Centennial hops, Swift is a bright and clean IPA with an intense citrus bouquet and flavor.', 2),
 
-beers (beer_name, style, abv, description, brew_id) VALUES ('People Power', 'American Pale Ale', 5.5, 'Wakatu hops lead the show here, bringing bright citrus and floral notes atop a soft base of pale malt and wheat. 10% of purchase donated to ACLU.', 2);
-INSERT INTO
-beers (beer_name, style, abv, description, brew_id) VALUES ('Family Saison', 'Saison', 5.9, 'Focused and simple with classic Belgian yeast character, including notes of banana and earthy spice. Finishes bone-dry with a soft body.', 2);
+('Sour/Fruit', 'Ya Filthy Animal', 'Kettle Sour', 6.5, 'Inspired by the warming flavors of mulled wine. Ya Filthy Animal is a kettle sour brewed with plum, black currants and mulling spices. Keep the change.', 2),
+
+('Pale Ale', 'Ride or Die', 'Pale Ale', 5.5, 'A mellow, sessionable pale ale with tropical fruit overtones and an undercurrent of citrus zest. Built for the dog days, but good no matter what.', 2),
+
+('IPA', 'Clique', 'Brut IPA', 6.5, 'A Brut IPA with big notes of citrus zest, followed by a bone-dry finish.', 2),
+
+('IPA', 'There There', 'Double India Pale Ale', 7.7, 'A juicy, dank DIPA soaked with heavy doses of Vic Secret and Wakatu hops and fermented with a fruity Norwegian yeast strain.', 2),
+
+('IPA', '24:37 Red IPA', 'India Pale Ale', 6.5, 'With a notable red tint from roasted malt, this IPA has a bright, clean hop aroma and bitterness. Dry-hopped with Sorachi Ace for a floral, lemongrass aroma.', 2),
+
+('Porter/Stout', 'Off-Black', 'Dark Lager', 5.5, 'A roasty and malty Schwarzbier that is kept in balance with a healthy dose of noble hops.', 2),
+
+('Pale Ale', 'People Power', 'American Pale Ale', 5.5, 'Wakatu hops lead the show here, bringing bright citrus and floral notes atop a soft base of pale malt and wheat. 10% of purchase donated to ACLU.', 2),
+
+('Farmhouse/Saison', 'Family Saison', 'Saison', 5.9, 'Focused and simple with classic Belgian yeast character, including notes of banana and earthy spice. Finishes bone-dry with a soft body.', 2);
