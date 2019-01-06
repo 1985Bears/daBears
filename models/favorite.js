9b9160313bd3a1ea4052ed9b0b04740360f04119
Kitty <39654422+kat3ng@users.noreply.github.com>
2019-01-05 18:11:36 -0600

    foreign keys

--- models/favorite.js
+++ models/favorite.js
@@ -11,13 +11,16 @@ module.exports = function (sequilize, Datatypes) {
                 len: [1]
             }
         },
-        user_id: {
-            type: Datatypes.INTEGER,
-            allowNull: false,
-            validate: {
-                len: [1]
+    });
+
+    Favorite.associate = function (models) {
+        // We're saying that a Favorite should belong to an Author
+        // A Favorite can't be created without an Author due to the foreign key constraint
+        Favorite.belongsTo(models.User, {
+            foreignKey: {
+                allowNull: false
             }
-        },
-    })
+        });
+    };
     return Favorite;
 };
