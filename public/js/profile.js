792f2569d96127708e72a02fe9db87b1631df453
bdeLong <bendelong45@gmail.com>
2019-10-27 23:45:25 -0500

    fixed api routes

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,38 +1,40 @@
-let profileCategory = '';
+let profileCategory = "";
 
-$.get("/api/users").then((res) => {
-  $("#user-name").text(res.firstName + " " + res.lastName)
-})
-
-$.get("/api/newprofile").then((res) => res.forEach(profile => {
-  let newProfileDiv = $("<div>")
-  let newProfileButton = $("<button>");
-  newProfileButton.addClass("flavor-profile btn btn-lg");
-  newProfileButton.text(profile.profileName);
-  newProfileButton.attr("id", profile.category);
-  let newDeleteButton = $("<button>");
-  newDeleteButton.text('X');
-  newDeleteButton.addClass('btn btn-danger btn-lg delete');
-  newDeleteButton.attr("id", profile.profileName);
-
-  newProfileDiv.append(newProfileButton, newDeleteButton);
-  $("#beer-profiles").append(newProfileDiv);
-}));
-
-$(document).on("click", ".flavor-profile", function () {
-  profileCategory = $(this).attr("id");
-  sessionStorage.setItem('category', profileCategory);
-  sessionStorage.getItem('category');
-  window.location.replace(`/recommendations`)
+$.get("/api/users").then(res => {
+  $("#user-name").text(res.firstName + " " + res.lastName);
 });
 
-$(document).on("click", ".delete", function () {
+$.get("/api/newprofile").then(res =>
+  res.forEach(profile => {
+    let newProfileDiv = $("<div>");
+    let newProfileButton = $("<button>");
+    newProfileButton.addClass("flavor-profile btn btn-lg");
+    newProfileButton.text(profile.profileName);
+    newProfileButton.attr("id", profile.category);
+    let newDeleteButton = $("<button>");
+    newDeleteButton.text("X");
+    newDeleteButton.addClass("btn btn-danger btn-lg delete");
+    newDeleteButton.attr("id", profile.profileName);
 
+    newProfileDiv.append(newProfileButton, newDeleteButton);
+    $("#beer-profiles").append(newProfileDiv);
+  })
+);
+
+$(document).on("click", ".flavor-profile", function() {
+  profileCategory = $(this).attr("id");
+  sessionStorage.setItem("category", profileCategory);
+  sessionStorage.getItem("category");
+  window.location.replace(`/recommendations`);
+});
+
+$(document).on("click", ".delete", function() {
   $.ajax({
     method: "DELETE",
     url: "/api/delete-profile/" + $(this).attr("id")
-  })
+  });
 
-  $(this).closest("div").remove();
-
+  $(this)
+    .closest("div")
+    .remove();
 });
